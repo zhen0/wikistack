5909467e9c470d052ed674f98f42c1aecd2c4a9d
Jenny Grange <jenniferhelengrange@gmail.com>
2019-03-07 16:14:13 -0500

    adds function to make title into slug

--- models/index.js
+++ models/index.js
@@ -4,20 +4,24 @@ const db = new Sequelize("postgres://localhost:5432/wikistack", {
 });
 
 const Page = db.define("page", {
-  title: { type: Sequelize.STRING, validate: { allowNull: false } },
-  slug: { type: Sequelize.STRING, validate: { allowNull: false } },
-  content: { type: Sequelize.TEXT, validate: { allowNull: false } },
+  title: { type: Sequelize.STRING, allowNull: false },
+  slug: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.TEXT, allowNull: false },
   status: Sequelize.ENUM("open", "closed")
 });
 
+Page.beforeValidate((pageInstance, optionsObject) => {
+  pageInstance.slug = pageInstance.title.replace(/\s+/g, "_").replace(/\W/, "");
+  //   console.log(pageInstance.slug);
+});
+
 const User = db.define("user", {
-  name: { type: Sequelize.STRING, validate: { allowNull: false } },
+  name: { type: Sequelize.STRING, allowNull: false },
   email: {
     type: Sequelize.STRING,
     validate: {
       unique: true,
-      isEmail: true,
-      allowNull: false
+      isEmail: true
     }
   }
 });
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const add = require("../views/addPage");
+const { Page } = require("../models/index");
 
 router.get("/", (req, res, next) => {
   //   console.log("redirecting");
   res.redirect("../");
 });
 
-router.post("/", (req, res, next) => {
-  res.json(req.body);
-});
-
 router.get("/add", (req, res, next) => {
   res.send(add());
+});
+
+router.post("/", async (req, res, next) => {
+  //   console.log(req.body);
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
